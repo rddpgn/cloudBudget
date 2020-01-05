d5ee4e8424eab3e8c0a26687a12432866ddec82e
rddpgn <rddpgn@gmail.com>
2020-01-05 22:31:54 +0300

    Закончена работа над проектом

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 let gulp =          require('gulp');
 let sass =          require('gulp-sass');
 let browserSync =   require('browser-sync');
-let concat =        require('gulp-concat');
 
 gulp.task('sass', gulp.series(function() {
     return gulp.src('./src/scss/style.scss')
